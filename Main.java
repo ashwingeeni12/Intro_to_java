63f0d8648104206e3efd207703a3417e2fd1d4d2
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-02-25 00:09:14 +0000

    short data type

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
 class Main {
   public static void main(String[] args) {
-char a_letter = 'A';
-char sLetter = 's';
-char hLetter = 'h';
-char wLetter = 'w'; 
-char iLetter = 'i';
-char nLetter = 'n';
-System.out.println("" + a_letter+sLetter+hLetter+wLetter+iLetter+nLetter);
+short sUno = 1288;
+System.out.println("" + sUno);
   }   
 }
