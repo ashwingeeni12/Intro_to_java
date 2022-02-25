ca017681f1469d0774f8129a47ec9628a1c5be06
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-02-25 00:28:14 +0000

    boolean data type

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
   public static void main(String[] args) {
-float fUno = 3.14159f;
-System.out.println(fUno);
+boolean qOne = true;
+boolean qTwo = false;
+System.out.println("Saturday is better than Sunday. " + qOne);
+System.out.println("Sunday is better than Saturday. " + qTwo);
   }   
 }
