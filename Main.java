f79b89ade3a94f0272dfc93daa650f9b5c866de9
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-02-25 00:30:45 +0000

    string data type

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 class Main {
   public static void main(String[] args) {
-boolean qOne = true;
-boolean qTwo = false;
-System.out.println("Saturday is better than Sunday. " + qOne);
-System.out.println("Sunday is better than Saturday. " + qTwo);
+String greeting = "How are you doing?";
+System.out.println(greeting);
   }   
 }
