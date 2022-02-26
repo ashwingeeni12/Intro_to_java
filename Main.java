b27689fa10f10138ebc0f58b147f02091a8d4236
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-02-26 01:42:16 +0000

    odd or even

--- Main.java
+++ Main.java
@@ -1,13 +1,8 @@
 class Main {
   public static void main(String[] args) {
-int varOne = 12;
-int varTwo = 11;
-int mult, sum, div, sub, mod;
-mult = varOne * varTwo;
-sum = varOne + varTwo;
-div = varOne / varTwo;
-sub = varTwo - varOne;
-mod = varOne % varTwo;
-System.out.println("sum = " + sum + " multiplication = " + mult + " division = " + div + " subtraction = " + sub + " modulus = " + mod);
+int varOne = 7; 
+int reminder = varOne%2;
+if (reminder == 0)
+{System.out.println("even");}else {System.out.println("odd");}
   }   
 }
