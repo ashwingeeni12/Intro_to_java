60eb67dfc3d377f8a41850bbc5f00f7b8695d70b
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-02-26 01:13:13 +0000

    explicit type cast

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
-String greeting = "How are you doing?";
-System.out.println(greeting);
+long a = 100L;
+int b = (int) a;
+System.out.println(""+ b);
   }   
 }
