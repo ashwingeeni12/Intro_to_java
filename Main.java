b451f6a6817110d9c9bf3706f33e85eeb99acbdf
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-03-12 03:24:52 +0000

    3 varible

--- Main.java
+++ Main.java
@@ -1,8 +1,37 @@
 class Main {
   public static void main(String[] args) {
-    int age = 20;
-    if (age >= 18){
-      System.out.println("This person is eligible for voting."); 
+    int var1 = 3;
+    int var2 = 2;
+    int var3 = 1;
+    if (var1 > var2 && var1 > var3) {
+      System.out.println("var 1 is the largest");
+      if (var2>var3){
+        System.out.println("var 2 is the middle");
+        System.out.println("var 3 is the smallest");
+      }else{
+        System.out.println("var 3 is the middle");
+        System.out.println("var 2 is the smallest");
+      }
     }
-  }   
+    if (var2 > var1 && var2 > var3) {
+      System.out.println("var 2 is the largest");
+      if (var1>var3){
+        System.out.println("var 1 is the middle");
+        System.out.println("var 3 is the smallest");
+      }else{
+        System.out.println("var 3 is the middle");
+        System.out.println("var 1 is the smallest");
+      }
+    }
+    if (var3 > var2 && var3>var1) {
+      System.out.println("var 3 is the largest");
+      if (var2>var1){
+        System.out.println("var 2 is the middle");
+        System.out.println("var 1 is the smallest");
+      }else{
+        System.out.println("var 1 is the middle");
+        System.out.println("var 2 is the smallest");
+      }
+    }
+  } 
 }
