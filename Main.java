71962ecf0fc2cb539fca952cffbf5fc2d5a59471
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-03-12 00:56:35 +0000

    number

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 class Main {
   public static void main(String[] args) {
-int org_num = 1232;
-int incre = ++org_num;
-long increment = ; 
-System.out.println(incre);
-  }    
+int varOne = 7;
+int varTwo = varOne++t; 
+if (varTwo == varOne) {
+  System.out.println("sucess");
+}
+else {
+  System.out.println("failure");
+}
+  }   
 }
