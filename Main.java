5761a749a2e39f0c6e0df3307ac76f8b6efb9df1
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-03-19 00:48:23 +0000

    continue statment

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 class Main {
   public static void main(String[] args) {
     for(int i = 1; i<=10; i++){
-      System.out.println(i);
-      if(i == 5){
-        break;
+      if(i>=5 && i<=8){
+        continue;
       }
+      System.out.println(i);
     }
   } 
 }
