0a0a1b736e9da7652d06d6070734ee0095baa382
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-03-19 00:25:01 +0000

    sum of natural numbers from 1 - 10

--- Main.java
+++ Main.java
@@ -1,37 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    int var1 = 3;
-    int var2 = 2;
-    int var3 = 1;
-    if (var1 > var2 && var1 > var3) {
-      System.out.println("var 1 is the largest");
-      if (var2>var3){
-        System.out.println("var 2 is the middle");
-        System.out.println("var 3 is the smallest");
-      }else{
-        System.out.println("var 3 is the middle");
-        System.out.println("var 2 is the smallest");
-      }
-    }
-    if (var2 > var1 && var2 > var3) {
-      System.out.println("var 2 is the largest");
-      if (var1>var3){
-        System.out.println("var 1 is the middle");
-        System.out.println("var 3 is the smallest");
-      }else{
-        System.out.println("var 3 is the middle");
-        System.out.println("var 1 is the smallest");
-      }
-    }
-    if (var3 > var2 && var3>var1) {
-      System.out.println("var 3 is the largest");
-      if (var2>var1){
-        System.out.println("var 2 is the middle");
-        System.out.println("var 1 is the smallest");
-      }else{
-        System.out.println("var 1 is the middle");
-        System.out.println("var 2 is the smallest");
-      }
+    int n = 10;
+    int sum = 0;
+    for(int i = 1; i<=n; i++){
+      sum = sum + i;
+      System.out.println(sum);
     }
   } 
 }
