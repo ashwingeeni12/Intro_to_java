2b8364315cf465b10f0c54a03a41d797973e91de
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-03-19 01:00:48 +0000

    even and odd sum

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    for(int i = 1; i<=10; i++){
-      if(i>=5 && i<=8){
-        continue;
+    int evenSum = 0, oddSum = 0;
+    for(int i=0; i<=10; i++){
+      if(i%2 == 0){
+        evenSum += i;        
+      }else{
+        oddSum += i;
       }
-      System.out.println(i);
     }
+    System.out.println("even sum = " + evenSum);
+    System.out.println("odd sum = " + oddSum);
   } 
 }
