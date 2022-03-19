12d7f97cc951d98982e4604f3eb53d460e7f335a
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-03-19 00:35:36 +0000

    while statment

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@ class Main {
   public static void main(String[] args) {
     int n = 10;
     int sum = 0;
-    for(int i = 1; i<=n; i++){
-      sum = sum + i;
+    int i = 10;
+    while(i<=n){
+      sum += i;
       System.out.println(sum);
+      i++;
     }
   } 
 }
