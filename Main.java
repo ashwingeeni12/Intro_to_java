74038275a66caa00c86534cff9743788b4ef95ac
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-03-19 00:43:24 +0000

    break statment

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    int n = 10;
-    int sum = 0;
-    int i = 10;
-    while(i<=n){
-      sum += i;
-      System.out.println(sum);
-      i++;
+    for(int i = 1; i<=10; i++){
+      System.out.println(i);
+      if(i == 5){
+        break;
+      }
     }
   } 
 }
