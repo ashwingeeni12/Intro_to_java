48f4cbe14828b8587b7ebc327d0a6d823b441172
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-03-31 21:49:36 +0000

    calculator

--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    int evenSum = 0, oddSum = 0;
-    for(int i=0; i<=10; i++){
-      if(i%2 == 0){
-        evenSum += i;        
-      }else{
-        oddSum += i;
-      }
+    Scanner inputOp = new Scanner(System.in);  
+    System.out.println("Enter Arithmetic Operation add, mult, div, sub");
+    String operation = inputOp.nextLine();
+    System.out.println("Enter 1 number");
+    int NumOne = inputOp.nextInt();
+    System.out.println("Enter Another Number");
+    int NumTwo = inputOp.nextInt();
+    System.out.println(operation);
+    if (operation.equals("mult")){
+     System.out.println(NumOne*NumTwo); 
+    } else if (operation.equals("div")){
+     System.out.println(NumOne/NumTwo); 
+    } else if (operation.equals("add")){
+     System.out.println(NumOne+NumTwo); 
+    } else if (operation.equals("sub")){
+     System.out.println(NumOne-NumTwo); 
     }
-    System.out.println("even sum = " + evenSum);
-    System.out.println("odd sum = " + oddSum);
   } 
 }
