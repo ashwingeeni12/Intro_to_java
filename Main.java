ab95ae6015a18581b9b7b7ada5dcafad8e9f8714
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-04-01 23:05:58 +0000

    class

--- Main.java
+++ Main.java
@@ -1,23 +1,6 @@
-import java.util.Scanner;
 
-class Main {
+class Person {
   public static void main(String[] args) {
-    Scanner inputOp = new Scanner(System.in);  
-    System.out.println("Enter Arithmetic Operation add, mult, div, sub");
-    String operation = inputOp.nextLine();
-    System.out.println("Enter 1 number");
-    int NumOne = inputOp.nextInt();
-    System.out.println("Enter Another Number");
-    int NumTwo = inputOp.nextInt();
-    System.out.println(operation);
-    if (operation.equals("mult")){
-     System.out.println(NumOne*NumTwo); 
-    } else if (operation.equals("div")){
-     System.out.println(NumOne/NumTwo); 
-    } else if (operation.equals("add")){
-     System.out.println(NumOne+NumTwo); 
-    } else if (operation.equals("sub")){
-     System.out.println(NumOne-NumTwo); 
-    }
+    
   } 
 }
