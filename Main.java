de4dd63ceaefa37ec9fd8805dfbdbc09266c2391
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-05-08 15:06:14 +0000

    factorial calculator

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner; //Extra 
 class Main {
   public static void main(String[] args) {
-    Person mom = new Person();
-    mom.walkForward();
-    mom.walkingBackward();
-  } 
+  Scanner factNum = new Scanner(System.in); //Extra 
+  System.out.println("What number do you want to factorialize?"); //Extra 
+  int var1 = factNum.nextInt();
+  var1 = factorial(var1);
+  System.out.println(var1);
+  factNum.close(); //Extra 
+    }
+  static int factorial(int one){
+    if (one == 1) 
+      return 1;
+    else 
+      return(one * factorial(one-1)); 
+  }
+
 }
