207764c45c145f8e2c95d895873a99b2f682222f
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-05-14 21:22:54 +0000

    int array sums

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    int count = 0;
-    Scanner inputString = new Scanner(System.in);  
-    String UserInput = inputString.nextLine();
-    for(int i = 0; i < UserInput.length(); i++){
-      if(UserInput.charAt(i) == ' '){
-        count++;
-      }      
+    int[] int_array = {3, 6, 7, 4, 10};
+    int sum = 0;
+        for (int i = 0; i < int_array.length; i++){
+          sum += int_array[i];
+        }
+        System.out.println(sum);
     }
-    System.out.println(UserInput.length() - count);
-  }
 }
