f807e828c793228a96863499da76a92d79cd2b13
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-05-28 01:12:27 +0000

    integer digits add

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    int count = 0;
-    Scanner inputString = new Scanner(System.in);  
-    String UserInput = inputString.nextLine();
-    for(int i = 0; i < UserInput.length(); i++){
-      if(UserInput.charAt(i) == 'a'|| UserInput.charAt(i) == 'e' || UserInput.charAt(i) == 'i' || UserInput.charAt(i) == 'o' || UserInput.charAt(i) == 'u' || UserInput.charAt(i) == 'A' || UserInput.charAt(i) == 'E' || UserInput.charAt(i) == 'I' || UserInput.charAt(i) == 'O' || UserInput.charAt(i) == 'U'){
-        count++;
-      }      
+    Scanner scanner = new Scanner(System.in);
+    int int_input = scanner.nextInt();
+    int sum = 0; 
+    while(int_input > 0){
+      sum += int_input%10;
+      int_input = int_input/10;
     }
-    System.out.println(count);
+    System.out.println(sum);
   }
 }
