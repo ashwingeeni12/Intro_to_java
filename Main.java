052d92bbb2cc117a1e0275f53f6687cf56f76a53
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-05-28 01:49:22 +0000

    int array smallest number

--- Main.java
+++ Main.java
@@ -1,23 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println(addition(10.2, 2.5)); 
-  }
-  public static int multiply(int num_, int num__){
-    return num_ * num__;
-  }
-  public static int addition(int num_, int num__){
-    return num_ + num__;
-  }
-  public static int division(int num_, int num__){
-    return num_ / num__;
-  }
-  public static int subtaction(int num_, int num__){
-    return num_ - num__;
-  }
-  public static double addition(double dec1, double dec2){
-    return dec1 + dec2;
-  }
-  public static int addition(int num_, int num__, int num___){
-    return num_ + num__ + num___;
+    int[] array1 = {5, 2, 10, 6, 8, 9, 22, 1000, -2, -10000};
+    int smallest = array1[0];
+    for(int i = 0; i < array1.length; i++){
+      if (array1[i] < smallest){
+        smallest = array1[i];
+      }
+    }
+    System.out.println(smallest);
   }
 }
