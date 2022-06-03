59797b0b0e2d811e91f19d1dd20f531ceeaf77a7
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-06-03 18:29:26 +0000

    box class unfinished p2

--- Box.java
+++ Box.java
@@ -2,4 +2,15 @@ class Box{
   int length = 2;
   int height = 2;
   int width = 2;
+
+  int BoxVol(){
+    return length*width*height;
+  }
+  int BoxVol(int cuboid){
+    cuboid = length = width = height;
+    return cuboid*cuboid*cuboid; 
+  }
+  int BoxVol(int l, int h, int w){
+    return l*h*w;
+  }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    
+    int Box_1 = new BoxVol();
+    int Box_2 = BoxVol(9);
+    int Box_3 = BoxVol(4, 6, 9);
+    System.out.println(Box_1);
+    System.out.println(Box_2);
+    System.out.println(Box_3);
   }
 }
