2e1dd6fcf238654b3b320e91c0757544b6bee37d
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-06-03 18:12:11 +0000

    box class unfinished

---
+++ Box.java
@@ -0,0 +1,5 @@
+class Box{
+  int length = 2;
+  int height = 2;
+  int width = 2;
+}
--- Main.java
+++ Main.java
@@ -1,12 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    int[] array1 = {5, 2, 10, 6, 8, 9, 22, 1000, -2, -10000};
-    int smallest = array1[0];
-    for(int i = 0; i < array1.length; i++){
-      if (array1[i] < smallest){
-        smallest = array1[i];
-      }
-    }
-    System.out.println(smallest);
+    
   }
 }
