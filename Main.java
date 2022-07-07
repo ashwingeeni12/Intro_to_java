c36db665f8f550cbccf4c0da1a33f46e130c3233
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-07 19:59:00 +0000

    nearest chapter

--- Main.java
+++ Main.java
@@ -1,36 +1,49 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    Scanner input_Int = new Scanner(System.in);
-    int input = input_Int.nextInt();
-    boolean prime = false;
-    int i = 1;
-    int[] array1 = new int[input];
-    int output = 2;
-    for(int a = 2; a >= 2; a++){
-      prime = primeC(a);
-      if(prime == true){
-        if(i < array1.length){
-          array1[i] = a;
-          output = output * array1[i];
-          i++;
-        } else{
-          System.out.println(output);
-          System.exit(0);
-        }
-      }
-    }
+    System.out.println("Enter page from 1 - 37");
+    Scanner input_Page = new Scanner(System.in);
+    int input = input_Page.nextInt();
+    int Chapter1 = 1;
+    int Chapter2 = 15;
+    int Chapter3 = 37;
+    System.out.println(Chapt(Chapter1, Chapter2, Chapter3, input));
   }
-  public static boolean primeC(int a){
-    int count = 0;
-    for(int b = 2; b < a; b++){
-      if(a % b != 0){
-        count++;
+  public static String Chapt(int Chapter1, int Chapter2, int Chapter3, int input){
+    int out = 0;
+    int[] array1 = new int[2];
+    int[] array2 = new int[2];
+    if(Chapter1 <= input && input < Chapter2){
+      out = input - Chapter1;
+      array1[0] = out;
+      out = Chapter2 - input;
+      array1[1] = out;
+      if(array1[0] >= array1[1]){
+        out = array1[0];
+        return "Chapter 2";
       }
-      if(count == a - 2){
-        return true; 
+      if(array1[0] < array1[1]){
+        out = array1[1];
+        return "Chapter 1";
       }
     }
-    return false;
+    if(Chapter2 <= input && input < Chapter3){
+      out = input - Chapter2;
+      array2[0] = out;
+      out = Chapter3 - input;
+      array2[1] = out;
+      if(array2[0] >= array2[1]){
+        out = array2[0];
+        return "Chapter 3";
+      }
+      if(array2[0] < array2[1]){
+        out = array2[1];
+        return "Chapter 2";
+      }
+    }
+    if(Chapter3 == input){
+      return "Chapter 3";
+    }
+    return "Out of Bounds"; 
   }
 }
