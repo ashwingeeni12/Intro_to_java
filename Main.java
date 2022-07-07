1f8c89ded65400766d885158b778e7bcc5007889
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-07 17:53:51 +0000

    primorial

--- Main.java
+++ Main.java
@@ -1,17 +1,36 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    int out = 0;
-    Scanner input_String = new Scanner(System.in);
-    String input = input_String.nextLine();
-    String alpha = "abcdefghijklnmopqrstuvwxyz";
-    for(int i = 0; i < input.length(); i++){
-      for(int a = 0; a < alpha.length(); a++){
-        if(input.charAt(i) == alpha.charAt(a)){
-          out += (a + 1);
+    Scanner input_Int = new Scanner(System.in);
+    int input = input_Int.nextInt();
+    boolean prime = false;
+    int i = 1;
+    int[] array1 = new int[input];
+    int output = 2;
+    for(int a = 2; a >= 2; a++){
+      prime = primeC(a);
+      if(prime == true){
+        if(i < array1.length){
+          array1[i] = a;
+          output = output * array1[i];
+          i++;
+        } else{
+          System.out.println(output);
+          System.exit(0);
         }
       }
     }
-    System.out.println(out / input.length());
-  } 
+  }
+  public static boolean primeC(int a){
+    int count = 0;
+    for(int b = 2; b < a; b++){
+      if(a % b != 0){
+        count++;
+      }
+      if(count == a - 2){
+        return true; 
+      }
+    }
+    return false;
+  }
 }
