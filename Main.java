85001fff23b795ab64343d34a622665701743742
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-07 00:16:40 +0000

    rock paper scissors

--- Main.java
+++ Main.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
-class Main {
+class Main{
   public static void main(String[] args) {
-    System.out.println("Enter Number");
-    Scanner sym = new Scanner(System.in);
-    int s = sym.nextInt();
-    String sy = String.valueOf(s);
-    char[] array = sy.toCharArray();
-    String out = "";
-    int finale = 0;
-    for(int i = sy.length() - 1; i >= 0; i--){
-      out += array[i];
+    System.out.println("Player One chose Rock, Paper, or Scissors.");
+    Scanner inputOne = new Scanner(System.in);
+    String input1 = inputOne.nextLine();
+    System.out.println("Player Two chose Rock, Paper, or Scissors.");
+    Scanner inputTwo = new Scanner(System.in);
+    String input2 = inputTwo.nextLine();
+
+    System.out.println(winner(input1, input2));
+  }
+  public static String winner(String input1, String input2){
+    String output = "test";
+    if(input1.equals(input2)){
+      output = "TIE";
+    } 
+    else if(input1.equals("Rock") && input2.equals("Scissors") || input1.equals("Scissors") && input2.equals("Paper") || input1.equals("Paper") && input2.equals("Scissors")){
+      output = "Player One Wins";
+    } 
+    else if(input2.equals("Rock") && input1.equals("Scissors") || input2.equals("Scissors") && input1.equals("Paper") || input2.equals("Paper") && input1.equals("Scissors")){
+      output = "Player 2 Wins";
     }
-    finale = Integer.valueOf(out);
-    if(finale == s){
-      System.out.println("This number is symetrical");
-    }
-    else{
-      System.out.println("This number is not symetrical");
-    }
-  } 
+  return output;
+  }
 }
