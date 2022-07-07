ec7436e5a93fcdc918702f9b4262f2d98d7c72db
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-07 00:55:29 +0000

    Average of Letters

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter Price ex. 99.01");
-    Scanner price = new Scanner(System.in);
-    double p = price.nextDouble();
-    System.out.println("Enter Discount ex 50.00");
-    Scanner discount = new Scanner(System.in);
-    double d = discount.nextDouble();
-    System.out.println(output(p, d));
+    int out = 0;
+    Scanner input_String = new Scanner(System.in);
+    String input = input_String.nextLine();
+    String alpha = "abcdefghijklnmopqrstuvwxyz";
+    for(int i = 0; i < input.length(); i++){
+      for(int a = 0; a < alpha.length(); a++){
+        if(input.charAt(i) == alpha.charAt(a)){
+          out += (a + 1);
+        }
+      }
+    }
+    System.out.println(out / input.length());
   } 
-  public static double output(double p, double d){
-    d = d * .01;
-    p = p - (p * d);
-    double price = Math.round(p * 100.0) / 100.0;
-    return price;
-  }
 }
