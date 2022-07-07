b97d23b947dac40bd6d5c7abd6c9cc1a9bd82d6b
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-07 00:18:00 +0000

    discount

--- Main.java
+++ Main.java
@@ -1,43 +1,18 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Programmer 1 Wage");
-    Scanner first = new Scanner(System.in);
-    int p1 = first.nextInt();
-    System.out.println("Programmer 2 Wage");
-    Scanner second = new Scanner(System.in);
-    int p2 = first.nextInt();
-    System.out.println("Programmer 3 Wage");
-    Scanner third = new Scanner(System.in);
-    int p3 = third.nextInt();
-
-    int large = 0;
-    int small = 0;
-    
-    if (p1 >= p2 && p1 >= p3) {
-      large = p1;
-      if (p2>=p3){
-        small = p3;
-      }else{
-        small = p2;
-      }
-    }
-    if (p2 >= p1 && p2 >= p3) {
-      large = p2;
-      if (p1>=p3){
-        small = p3;
-      }else{
-        small = p1;
-      }
-    }
-    if (p3 >= p2 && p3 >= p1) {
-      large = p3;
-      if (p2 >= p1){
-        small = p1;
-      }else{
-        small = p2;
-      }
-    }
-  System.out.println(large - small);
+    System.out.println("Enter Price ex. 99.01");
+    Scanner price = new Scanner(System.in);
+    double p = price.nextDouble();
+    System.out.println("Enter Discount ex 50.00");
+    Scanner discount = new Scanner(System.in);
+    double d = discount.nextDouble();
+    System.out.println(output(p, d));
   } 
+  public static double output(double p, double d){
+    d = d * .01;
+    p = p - (p * d);
+    double price = Math.round(p * 100.0) / 100.0;
+    return price;
+  }
 }
