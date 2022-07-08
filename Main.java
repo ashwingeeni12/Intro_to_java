7a7e0d42547dcf9cb8a8603217ba8f726cdf231b
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-08 15:40:00 +0000

    Fibonacci

--- Main.java
+++ Main.java
@@ -1,28 +1,45 @@
 import java.util.Scanner;
+import java.util.Arrays;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter 5 numbers");
-    Scanner input_Num1 = new Scanner(System.in);
-    int num1 = input_Num1.nextInt();
-    Scanner input_Num2 = new Scanner(System.in);
-    int num2 = input_Num2.nextInt();
-    Scanner input_Num3 = new Scanner(System.in);
-    int num3 = input_Num3.nextInt();
-    Scanner input_Num4 = new Scanner(System.in);
-    int num4 = input_Num4.nextInt();
-    Scanner input_Num5 = new Scanner(System.in);
-    int num5 = input_Num5.nextInt();
-    int[] array1 = new int[5];
-    array1[0] = num1;
-    array1[1] = num2;
-    array1[2] = num3;
-    array1[3] = num4;
-    array1[4] = num5;
-    if(array1[1] % array1[0] == 0 && array1[2] % array1[1] == 0 && array1[3] % array1[2] == 0 && array1[4] % array1[3] == 0){
-      System.out.println("This is a factor chain");
+    Scanner input_ = new Scanner(System.in);
+    int input = input_.nextInt();
+    int output = fib(input);
+    if(output <= 0){
+      System.out.println("Error");
     }
     else{
-      System.out.println("This is not a factor chain");
+      System.out.println(output);
     }
+  }
+  public static int fib(int input){
+    int max = 0;
+    int first = 1;
+    int second = 1;
+    int third = first + second;
+    int[] array1 = new int[input];
+    array1[0] = first;
+    if(input == 1){
+      return second;
+    }
+    array1[1] = second;
+    if(input == 2){
+      return third;
+    }
+    array1[2] = third;
+    if(input == 3){
+      return third + second;
+    }
+    for(int i = 3; i < array1.length; i++){
+      first = second;
+      second = third;
+      third = first + second;
+      array1[i] = third;
+      if(i == array1.length - 1){
+        max = array1[input - 1] + array1[input - 2];
+        break;
+      }
+    }
+  return max;
   }
 }
