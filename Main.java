bc1bbedaaaed1405138e1d7fba718978a74f605a
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-08 17:04:23 +0000

    mean of all digits

--- Main.java
+++ Main.java
@@ -1,45 +1,13 @@
 import java.util.Scanner;
-import java.util.Arrays;
 class Main {
   public static void main(String[] args) {
     Scanner input_ = new Scanner(System.in);
-    int input = input_.nextInt();
-    int output = fib(input);
-    if(output <= 0){
-      System.out.println("Error");
+    String input = input_.nextLine();
+    char[] array1 = input.toCharArray();
+    int out = 0;
+    for(int i = 0; i < input.length(); i++){
+      out += Character.getNumericValue(array1[i]);
     }
-    else{
-      System.out.println(output);
-    }
-  }
-  public static int fib(int input){
-    int max = 0;
-    int first = 1;
-    int second = 1;
-    int third = first + second;
-    int[] array1 = new int[input];
-    array1[0] = first;
-    if(input == 1){
-      return second;
-    }
-    array1[1] = second;
-    if(input == 2){
-      return third;
-    }
-    array1[2] = third;
-    if(input == 3){
-      return third + second;
-    }
-    for(int i = 3; i < array1.length; i++){
-      first = second;
-      second = third;
-      third = first + second;
-      array1[i] = third;
-      if(i == array1.length - 1){
-        max = array1[input - 1] + array1[input - 2];
-        break;
-      }
-    }
-  return max;
+    System.out.println(out/input.length());
   }
 }
