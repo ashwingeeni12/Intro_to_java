eabea44002a35afcef9a1af7bcdf16b0c2e1074b
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-09 01:32:39 +0000

    fib 2

--- Main.java
+++ Main.java
@@ -1,28 +1,14 @@
-import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    Scanner input_ = new Scanner(System.in);
-    String input = input_.nextLine();
-    char[] array1 = input.toCharArray();
-    int out = 0;
-    for(int i = 0; i < input.length(); i++){
-      out += Character.getNumericValue(array1[i]);
-    }
-    System.out.println(out/input.length());
-    System.out.println(mean(666));
+    System.out.println(Fibonacci(3));
   }
-  public static int mean(int n){
-    int mean = 0; 
-    int digit = 0;
-    int total = 0;
-    int count = 0;
-    while(n > 0){
-      digit = n % 10;
-      total += digit;
-      n = n / 10;
-      count += 1;
+  public static int Fibonacci(int n){
+    int[] series = new int[n + 1];
+    series[0] = 1;
+    series[1] = 1;
+    for(int i = 2; i < n + 1; i++){
+      series[i] = series[i-1] + series[i-2];
     }
-    mean = total/count;
-    return mean;
+    return series[n];
   }
 }
