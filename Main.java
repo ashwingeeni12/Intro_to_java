3a916c22d99a3ed32a27cab63397924c03ad0890
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-09 01:19:17 +0000

    mean 2

--- Main.java
+++ Main.java
@@ -1,48 +1,28 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    // double output = 0;
-    // double[] array1 = new double[4];
-    // Scanner input_1 = new Scanner(System.in);
-    // double input = input_1.nextDouble();
-    // array1[0] = input;
-    // Scanner input_2 = new Scanner(System.in);
-    // double input1 = input_2.nextDouble();
-    // array1[1] = input1;
-    // Scanner input_3 = new Scanner(System.in);
-    // double input2 = input_3.nextDouble();
-    // array1[2] = input2;
-    // Scanner input_4 = new Scanner(System.in);
-    // double input3 = input_4.nextDouble();
-    // array1[3] = input3;
-    // for(int i = 0; i < array1.length; i++){
-    //   output += array1[i];
-    // }
-    // output = output/array1.length;
-    // int out = (int)output;
-    // System.out.println(check(out, output));
-    int[] num = {1, 2, 3, 4};
-    System.out.println(isAvg(num));
+    Scanner input_ = new Scanner(System.in);
+    String input = input_.nextLine();
+    char[] array1 = input.toCharArray();
+    int out = 0;
+    for(int i = 0; i < input.length(); i++){
+      out += Character.getNumericValue(array1[i]);
+    }
+    System.out.println(out/input.length());
+    System.out.println(mean(666));
   }
-  // public static boolean check(int out, double output){
-  //   if(out < output && output < out + 1){
-  //     return false;
-  //   }
-  //   return true;
-  // }
-  public static boolean isAvg(int[] nums){
-    double total = 0.0;
-    double avg = 0.0;
-    double x = 0.0;
-
-    for(int i = 0; i < nums.length; i++){
-      total += nums[i];
+  public static int mean(int n){
+    int mean = 0; 
+    int digit = 0;
+    int total = 0;
+    int count = 0;
+    while(n > 0){
+      digit = n % 10;
+      total += digit;
+      n = n / 10;
+      count += 1;
     }
-    avg = toal/nums.length;
-    x = (avg * 10) % 10;
-    if(x == 0){
-      return true;
-    }
-    return false;
+    mean = total/count;
+    return mean;
   }
 }
