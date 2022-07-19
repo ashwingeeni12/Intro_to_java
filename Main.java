d246c0d53bd1316e56616281be63a7c091cf6333
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-19 21:54:44 +0000

    Product of Remaining Elements

--- Main.java
+++ Main.java
@@ -2,48 +2,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 class Main {
   public static void main(String[] args){
-    ArrayList<Integer> unsorted = new ArrayList<Integer>();
-    Collections.addAll(unsorted, 1, 2, 3, 4);
-    System.out.println(reorder(unsorted, "asc"));
+    ArrayList <Integer> list = new ArrayList <Integer>();
+    Collections.addAll(list, -1, -10, 1, -2, 20);
+    System.out.println(canPartition(list));
   }
-  public static ArrayList<Integer> reorder(ArrayList<Integer> unsorted, String type){
-    ArrayList<Integer> sorted = new ArrayList<Integer>();
-    ArrayList<Integer> sort = new ArrayList<Integer>();
-    String final_ = "";
-    if(type == "asc"){
-      for(int i = 0; i < unsorted.size(); i++){
-        String convert = unsorted.get(i).toString();
-        for(int a = 0; a < convert.length(); a++){
-          sort.add(Character.getNumericValue(convert.charAt(a)));
-        }
-        Collections.sort(sort);
-        for(int a = 0; a < sort.size(); a++){
-          final_ = final_ + sort.get(a);
-        }
-        System.out.println(final_);
-        int output = Integer.parseInt(final_);
-        sorted.add(output);
-        sort.clear();
-        final_ = "";
-      }
+  public static boolean canPartition(ArrayList<Integer> pre){
+    Collections.sort(pre, Collections.reverseOrder());
+    int largest = pre.get(0);
+    int largest2 = pre.get(pre.size() - 1);
+    int final_1 = 1;
+    int final_2 = 1;
+    boolean output = false;
+    for(int a = 1; a < pre.size(); a++){
+      final_1 = final_1 * pre.get(a);
     }
-    if(type == "desc"){
-      for(int i = 0; i < unsorted.size(); i++){
-        String convert = unsorted.get(i).toString();
-        for(int a = 0; a < convert.length(); a++){
-          sort.add(Character.getNumericValue(convert.charAt(a)));
-        }
-        Collections.sort(sort, Collections.reverseOrder());
-        for(int a = 0; a < sort.size(); a++){
-          final_ = final_ + sort.get(a);
-        }
-        System.out.println(final_);
-        int output = Integer.parseInt(final_);
-        sorted.add(output);
-        sort.clear();
-        final_ = "";
-      }
+    if(final_1 == largest){
+      output = true;
     }
-    return sorted;
-  } 
+    for(int a = 0; a < pre.size() - 1; a++){
+      final_2 = final_2 * pre.get(a);
+    }
+    if(final_2 == largest2){
+      output = true;
+    }
+    return output;
+  }
 }
