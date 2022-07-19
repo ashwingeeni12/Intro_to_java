dd1ec8e171a5dec57e20f20de9e744709ac1d2fc
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-19 19:41:51 +0000

    Digits Battle

--- Main.java
+++ Main.java
@@ -1,14 +1,37 @@
+import java.util.ArrayList;
 class Main {
-  public static void main(String[] args) {
-    System.out.println(Fibonacci(3));
+  public static void main(String[] args){
+    System.out.println(battle(78925));
   }
-  public static int Fibonacci(int n){
-    int[] series = new int[n + 1];
-    series[0] = 1;
-    series[1] = 1;
-    for(int i = 2; i < n + 1; i++){
-      series[i] = series[i-1] + series[i-2];
+  public static ArrayList<Integer> battle(int input){
+    int i = 0;
+    int a = 0;
+    String intp = String.valueOf(input);
+    int output1 = 0;
+    ArrayList<Integer> out = new ArrayList<Integer>();
+    ArrayList<Integer> finale = new ArrayList<Integer>();
+    for(int b = 0; b < intp.length(); b++){
+      output1 = Character.getNumericValue(intp.charAt(b));
+      out.add(output1);
     }
-    return series[n];
-  }
+    while(i < intp.length()){
+      System.out.println(out.get(i));
+      a = i + 1;
+      if(a >= intp.length()){
+        finale.add(out.get(i));
+        return finale;
+      }
+      if(out.get(i) > out.get(a)){
+         finale.add(out.get(i));
+         System.out.println("called1");
+      } else if(out.get(i) < out.get(a)){
+        finale.add(out.get(a));
+        System.out.println("called2");
+      } else if(out.get(i) == out.get(a)){
+        System.out.println("Skipped");
+     }
+     i = i + 2;
+   }
+  return finale;
+ }
 }
