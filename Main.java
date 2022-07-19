aa8304b0cb11b6d10f97e8fdec6e41d95ac0b401
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-19 20:38:50 +0000

    Reorder Digits

--- Main.java
+++ Main.java
@@ -1,37 +1,49 @@
 import java.util.ArrayList;
+import java.util.Collections;
 class Main {
   public static void main(String[] args){
-    System.out.println(battle(78925));
+    ArrayList<Integer> unsorted = new ArrayList<Integer>();
+    Collections.addAll(unsorted, 1, 2, 3, 4);
+    System.out.println(reorder(unsorted, "asc"));
   }
-  public static ArrayList<Integer> battle(int input){
-    int i = 0;
-    int a = 0;
-    String intp = String.valueOf(input);
-    int output1 = 0;
-    ArrayList<Integer> out = new ArrayList<Integer>();
-    ArrayList<Integer> finale = new ArrayList<Integer>();
-    for(int b = 0; b < intp.length(); b++){
-      output1 = Character.getNumericValue(intp.charAt(b));
-      out.add(output1);
-    }
-    while(i < intp.length()){
-      System.out.println(out.get(i));
-      a = i + 1;
-      if(a >= intp.length()){
-        finale.add(out.get(i));
-        return finale;
+  public static ArrayList<Integer> reorder(ArrayList<Integer> unsorted, String type){
+    ArrayList<Integer> sorted = new ArrayList<Integer>();
+    ArrayList<Integer> sort = new ArrayList<Integer>();
+    String final_ = "";
+    if(type == "asc"){
+      for(int i = 0; i < unsorted.size(); i++){
+        String convert = unsorted.get(i).toString();
+        for(int a = 0; a < convert.length(); a++){
+          sort.add(Character.getNumericValue(convert.charAt(a)));
+        }
+        Collections.sort(sort);
+        for(int a = 0; a < sort.size(); a++){
+          final_ = final_ + sort.get(a);
+        }
+        System.out.println(final_);
+        int output = Integer.parseInt(final_);
+        sorted.add(output);
+        sort.clear();
+        final_ = "";
       }
-      if(out.get(i) > out.get(a)){
-         finale.add(out.get(i));
-         System.out.println("called1");
-      } else if(out.get(i) < out.get(a)){
-        finale.add(out.get(a));
-        System.out.println("called2");
-      } else if(out.get(i) == out.get(a)){
-        System.out.println("Skipped");
-     }
-     i = i + 2;
-   }
-  return finale;
- }
+    }
+    if(type == "desc"){
+      for(int i = 0; i < unsorted.size(); i++){
+        String convert = unsorted.get(i).toString();
+        for(int a = 0; a < convert.length(); a++){
+          sort.add(Character.getNumericValue(convert.charAt(a)));
+        }
+        Collections.sort(sort, Collections.reverseOrder());
+        for(int a = 0; a < sort.size(); a++){
+          final_ = final_ + sort.get(a);
+        }
+        System.out.println(final_);
+        int output = Integer.parseInt(final_);
+        sorted.add(output);
+        sort.clear();
+        final_ = "";
+      }
+    }
+    return sorted;
+  } 
 }
