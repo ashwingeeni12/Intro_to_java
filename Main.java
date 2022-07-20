890c6f51406297c6498124258c2fa9cff484fede
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-20 18:17:21 +0000

    Numbered Cards

--- Main.java
+++ Main.java
@@ -2,27 +2,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 class Main {
   public static void main(String[] args){
-    ArrayList <Integer> list = new ArrayList <Integer>();
-    Collections.addAll(list, -1, -10, 1, -2, 20);
-    System.out.println(canPartition(list));
+    ArrayList <Integer> my_cards = new ArrayList<Integer>();
+    Collections.addAll(my_cards, 2, 5, 2, 6, 9);
+    ArrayList <Integer> your_cards = new ArrayList<Integer>();
+    Collections.addAll(your_cards, 3, 7, 3, 1, 2);
+    System.out.println(prodOfRemEle(my_cards, your_cards));
   }
-  public static boolean canPartition(ArrayList<Integer> pre){
-    Collections.sort(pre, Collections.reverseOrder());
-    int largest = pre.get(0);
-    int largest2 = pre.get(pre.size() - 1);
-    int final_1 = 1;
-    int final_2 = 1;
+  public static boolean prodOfRemEle(ArrayList<Integer> cards1, ArrayList<Integer> cards2){
     boolean output = false;
-    for(int a = 1; a < pre.size(); a++){
-      final_1 = final_1 * pre.get(a);
-    }
-    if(final_1 == largest){
-      output = true;
-    }
-    for(int a = 0; a < pre.size() - 1; a++){
-      final_2 = final_2 * pre.get(a);
-    }
-    if(final_2 == largest2){
+    int largest1_1 = 0;
+    int largest1_2 = 0;
+    int largest2_1 = 0;
+    int largest2_2 = 0;
+    Collections.sort(cards1, Collections.reverseOrder());
+    Collections.sort(cards2, Collections.reverseOrder());
+    largest1_1 = cards1.get(0);
+    largest1_2 = cards1.get(1);
+    largest2_1 = cards2.get(0);
+    largest2_2 = cards2.get(1);
+    String card1_outputPre = String.valueOf(largest1_1) + String.valueOf(largest1_2);
+    String card2_outputPre = String.valueOf(largest2_1) + String.valueOf(largest2_2);
+    int card1_outputF = Integer.valueOf(card1_outputPre);
+    int card2_outputF = Integer.valueOf(card2_outputPre);
+    if(card1_outputF > card2_outputF){
       output = true;
     }
     return output;
