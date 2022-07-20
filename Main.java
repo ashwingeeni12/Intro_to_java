c6ebb193c48d9a3c9cde5d801edab0c2841e68ee
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-20 22:12:50 +0000

    Array Of Multiples

--- Main.java
+++ Main.java
@@ -2,27 +2,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 class Main {
   public static void main(String[] args){
-    ArrayList <Integer> nums = new ArrayList<Integer>();
-    Collections.addAll(nums, 9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5);
-    System.out.println(LargestGap(nums));
+    System.out.println(multiple(7, 5));
   }
-  public static int LargestGap(ArrayList<Integer> nums){
-    int fcount = 1;
-    int gap = 0;
-    int gapT = 0;
-    Collections.sort(nums);
-    for(int i = 0; i < nums.size() - 1; i++){
-      gapT = nums.get(fcount) - nums.get(i);
-      if(gap < gapT){
-        gap = gapT;
-      }
-      if(fcount < nums.size()){
-        fcount++;
-      }
-      else{
-        break;
-      }
+  public static ArrayList<Integer> multiple(int num, int length){
+    ArrayList <Integer> multiples = new ArrayList<Integer>();
+    for(int i = 1; i <= length; i++){
+      multiples.add(num * i);
     }
-    return gap;
+    return multiples;
   }
 }
